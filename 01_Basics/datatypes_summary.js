4677d5d687aaf3703f4280b9d9386e49cc2ef096
SutraNayan <nirjhor73@gmail.com>
2024-08-22 04:38:19 +0000

    datatypes summary

---
+++ 01_Basics/datatypes_summary.js
@@ -0,0 +1,22 @@
+// Primitive DataType/ call by variables
+//  7 types, String, Number, Boolean, Null, Undefined, BigInt, Symbol.
+
+// Non-Primitive DataType/Reference Type/ call by reference
+
+//Array, Objects, Functions
+
+let heros = ["Strange", "Spider-Man", "Captain"];
+
+let myObj = {
+    name: "Nayan",
+    age: 29,
+};
+
+const myFunction = function(){
+    console.log("Hello World")
+};
+// How to find out the data types;== we can use the "typeof" to find out the type of the variable.
+
+
+console.log(typeof myObj);
+
