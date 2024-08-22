197139eb7693a3521e7fee867de2f0c8a15864b5
SutraNayan <nirjhor73@gmail.com>
2024-08-22 01:24:15 +0000

    conversion of dataTypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+let score = undefined;
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(valueInNumber);
+//  "33"=> 33
+// "33abc"=> NaN
+// ture=>1, false=>0
+// "" => false
+// "nayan" =>true
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log( typeof stringNumber);
