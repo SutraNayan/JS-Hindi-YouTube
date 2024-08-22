5cec99640ca8106bcafb7bf24904b732a3102bd8
SutraNayan <nirjhor73@gmail.com>
2024-08-22 01:07:25 +0000

    dataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+// "use strict"--this treat all JavaScript code as newer version.
+
+// alert(3+3); It only works in browser but we are using node.js that's why it throws an error.
+
+let name = "Nayan";
+let age = 29;
+let isLoggedIn = "true";
+
+// dataTypes
+/*1. Number
+2. String
+3. Boolean
+4. bigInt
+5. Null
+6. NaN
+7. undefined
+8. symbol
+*/
