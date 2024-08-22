5ebedc5a93ebe6c73103c032f31c411c73c0e2fe
SutraNayan <nirjhor73@gmail.com>
2024-08-22 01:49:34 +0000

    conversion

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -1,4 +1,4 @@
-let score = undefined;
+/*let score = undefined;
 
 console.log(typeof score);
 console.log(typeof(score));
@@ -17,4 +17,26 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 33;
 let stringNumber = String(someNumber);
-console.log( typeof stringNumber);
+console.log( typeof stringNumber);*/
+
+//**************************Opeations ***********************************
+
+let value = 3;
+let negValue = - value;
+console.log(negValue);
+//console.log(2+2);--Addition
+//console.log(2-2);--Subtraction
+//console.log(2*2);--Multiplication
+//console.log(4/2);--Division
+//console.log(2**3);--Exponent
+//console.log(2%3);--Modulus
+
+let str1 = "Hello";
+let str2 = " Nayan";
+let str3 = str1 + str2;
+console.log(str3);
+
+//What is the difference between prefix and postfix increment operator?
+//If used postfix, with operator after operand (for example, x++), the increment operator increments and returns the value before incrementing.
+
+//If used prefix, with operator before operand (for example, ++x), the increment operator increments and returns the value after incrementing.
