b13473fae17b0ed595c146c0997ff89069c6c9ce
SutraNayan <nirjhor73@gmail.com>
2024-08-22 00:49:10 +0000

    changes

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -7,7 +7,8 @@ let accountState;
 // accountId = 2; // if we declare a variable by using 'cosnt' then we can't change the assigned values.
 
 /* We will avoid to use "var" to declare variable because it makes problems with the blockscope in javasScript.
-   "let" we use this to assign variables which we can change in the future.*/ 
+   "let" we use this to assign variables which we can change in the future.
+   if a variable is not assigned the output is going to be undefined*/ 
 
 accountEmail = "hc@email.com";
 //console.log(accountEmail);
