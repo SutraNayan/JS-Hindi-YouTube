d8b05075db928a8a2dcbd5619d604be6c36b9361
SutraNayan <nirjhor73@gmail.com>
2024-08-22 05:14:07 +0000

    String

---
+++ 01_Basics/05_string.js
@@ -0,0 +1,6 @@
+const name ="Nayan";
+const repoCount = 50;
+// console.log(name +   repoCount + " Value");
+// for concatenation of string using (`) is the modern method. place the variable inside of {} by using a $ in front. like ${}.== String Interpulation
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
