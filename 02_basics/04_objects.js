c7ff20a8cad8aaccbe8e74068605f5d60c68bb0e
SutraNayan <nirjhor73@gmail.com>
2024-08-23 16:01:17 +0000

    object

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -33,7 +33,7 @@ const obj2 = {
  //const obj3 = Object.assign({},obj1, obj2);
  //console.log(obj3);
 const obj3 = {...obj1,...obj2};
-console.log(obj3);
+//console.log(obj3);
 
 
 const users = [
@@ -48,10 +48,21 @@ const users = [
 ]
 
 users[1].email
-console.log(tinderUser);
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty("isLogged"));
+// console.log(tinderUser.hasOwnProperty("isLogged"));
  
+
+
+// const course =  {
+//     coursename: "js in hindi",
+//     price: "999",
+//     courseInstructor: "hitesh"
+// }
+
+// const {courseInstructor: instructor} = course;// object destructuring 
+// console.log(instructor);
+
