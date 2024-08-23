d34fa4ea8e8553045c59cbf43d13cf7dbc824082
SutraNayan <nirjhor73@gmail.com>
2024-08-23 15:46:25 +0000

    objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -30,7 +30,28 @@ const obj2 = {
 }
  //const obj3 = {obj1, obj2};
 
- const obj3 = Object.assign({},obj1, obj2);
+ //const obj3 = Object.assign({},obj1, obj2);
  //console.log(obj3);
+const obj3 = {...obj1,...obj2};
+console.log(obj3);
 
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+            id: 1,
+            email: "h@gmail.com"
+    }
+]
+
+users[1].email
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty("isLogged"));
  
