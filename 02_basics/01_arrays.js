0f3dc7ebc2ea33c549ca8c97b55648d14406129d
SutraNayan <nirjhor73@gmail.com>
2024-08-23 03:57:00 +0000

    array

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,8 +1,8 @@
 //Arrays
 
-const myArr = [0, 1, 2, 3, 4, 5,];
-const myHeros = ["Nayan", "Brigitte"];
-const myArr2 = new Array(1, 2 , 3, 4);
+// const myArr = [0, 1, 2, 3, 4, 5,];
+// const myHeros = ["Nayan", "Brigitte"];
+// const myArr2 = new Array(1, 2 , 3, 4);
 //console.log(myArr[1]);
 
 // Array Methods
@@ -25,11 +25,29 @@ const myArr2 = new Array(1, 2 , 3, 4);
 //slice, splice
 
 
-console.log("A ", myArr);
+// console.log("A ", myArr);
 
-const myn1 = myArr.slice(1, 3);
-console.log(myn1);
-console.log("B ", myArr);
+// const myn1 = myArr.slice(1, 3);
+// console.log(myn1);
+// console.log("B ", myArr);
 
-const myn2 = myArr.splice(1,3);
-console.log(myn2);
+// const myn2 = myArr.splice(1,3);
+// console.log(myn2);
+
+
+const marvel_heros = ["Thor", "Ironman", "Spiderman"];
+const dc_heros = ["Superman", "Flash", "Batman"];
+//marvel_heros.push(dc_heros);
+//const allHeros = marvel_heros.concat(dc_heros);
+//const all_new_heros = [...marvel_heros, ...dc_heros];
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]];
+
+const real_another_array = another_array.flat(Infinity);
+
+console.log(Array.isArray("Nayan"));
+
+console.log(Array.from("Nayan"));
+console.log(real_another_array);
+
+
