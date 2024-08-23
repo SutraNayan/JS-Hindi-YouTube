66cc5341057bd65eff52ce0ad34e93c11cb655f0
SutraNayan <nirjhor73@gmail.com>
2024-08-23 04:43:44 +0000

    object

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,30 +1,30 @@
-// object literal
+// // object literal
 
- const js_user = {
-    name: "Nayan",
-    "full name": "Nayan Sutradahr",
-    age: 29,
-    location: "Norcross",
-    email: "n@gmail.com",
-    isLoggedIn: false,
-    lastLoginDays: ["Monday", "Saturday"]
- }
- console.log(js_user["full name"]);// square notation to access keys in string
- console.log(js_user["email"]);
+//  const js_user = {
+//     name: "Nayan",
+//     "full name": "Nayan Sutradahr",
+//     age: 29,
+//     location: "Norcross",
+//     email: "n@gmail.com",
+//     isLoggedIn: false,
+//     lastLoginDays: ["Monday", "Saturday"]
+//  }
+//  console.log(js_user["full name"]);// square notation to access keys in string
+//  console.log(js_user["email"]);
  
 
 
- js_user.email = "nayan@gmail.com";
- //Object.freeze(js_user);
- js_user.email = "sutra@gmail.com";
- //console.log(js_user);
+//  js_user.email = "nayan@gmail.com";
+//  //Object.freeze(js_user);
+//  js_user.email = "sutra@gmail.com";
+//  //console.log(js_user);
 
  
- js_user.greeting = function(){
-    console.log("Hello JS user");
- }
- js_user.greetingTwo = function(){
-    console.log(`Hello JS user, ${this.name}`);
- }
- console.log(js_user.greeting());
- console.log(js_user.greetingTwo());
+//  js_user.greeting = function(){
+//     console.log("Hello JS user");
+//  }
+//  js_user.greetingTwo = function(){
+//     console.log(`Hello JS user, ${this.name}`);
+//  }
+//  console.log(js_user.greeting());
+//  console.log(js_user.greetingTwo());
---
+++ 02_basics/04_objects.js
@@ -0,0 +1,9 @@
+const tinderUser = new Object();// singleton object
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy";
+tinderUser.isLoggedIn = false;
+
+
+
+console.log(tinderUser);
