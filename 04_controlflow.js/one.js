1a108059f3111c542048205ed042970ff0247a61
SutraNayan <nirjhor73@gmail.com>
2024-08-26 22:44:45 +0000

    if else statement

---
+++ 04_controlflow.js/one.js
@@ -0,0 +1,46 @@
+// if statement
+// const isUserLoogedIn = true;
+// const temperature = 41;
+
+// if(temperature === 40){
+//  console.log("Less Than 50");
+// }else{
+
+// console.log("Temperature is greater than 50");
+// };
+// console.log("Execute");
+
+// const score = 200;
+//  if(score>100){
+//     const power = "fly";
+//     console.log(`User Power: ${power}`);
+//  }
+//  console.log(`User Power ${power}`);
+
+// const balance = 1000;
+// if(balance>500) console.log("test");
+
+
+// const balance = 1000;
+
+// if(balance < 500){
+//     console.log("less than 500");
+// }else if(balance < 750) {
+//     console.log("less than 750");
+// }else if(balance < 900){
+//     console.log("less than 900");
+// }else{
+//     console.log("less than 1200");
+// }
+
+// const isUserLoogedIn = true;
+// const debitCard = true;
+// const loggedInFromGoogle = false;
+// const loggedInFromEmail = true;
+
+// if(isUserLoogedIn && debitCard && 2==3){
+//     console.log("Allow to buy courses");
+// }
+//  if(loggedInFromGoogle || loggedInFromEmail){
+//     console.log("User Looged in");
+//  }
