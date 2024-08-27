52b45f84800e018bee9bbd1abb053479d9835215
SutraNayan <nirjhor73@gmail.com>
2024-08-27 00:48:45 +0000

    loops

---
+++ 05_iteration.js/special_loop.js
@@ -0,0 +1,36 @@
+ // for of
+
+//  ["", "", ""];
+//  [{}, {}, {}];
+
+// const arr = [1, 2, 3, 4, 5];
+// for (const num of arr) {
+//     console.log(num);
+    
+// }
+
+// const grettings = "Hello World";
+// for (const greet of grettings) {
+//     console.log(`Each char is ${greet}`);
+    
+// }
+
+//maps
+
+// const map = new Map();
+// map.set("IN","India");
+// map.set("USA", "United States Of America");
+// console.log(map);
+
+// for (const [key, value] of map) {
+//         console.log(key, ":-", value);
+// }
+
+// const myObject = {
+//     "game1": "NFS",
+//     "game2": "Spiderman"
+// }
+
+// for (const [key, value] of myObject) {/// this doesn't work for for of loop.
+//     console.log(key, ":-", value);
+// }
